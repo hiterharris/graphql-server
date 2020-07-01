318292ddfe3e69b07bb1a0a1ad6bdcdd293fac85
Hiter Harris <hiterharris@comcast.net>
2020-07-01 16:12:01 -0400

    set up graphql server

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+const app = express();
+const PORT = 3001;
+
+const users = [
+    {
+        id: 1,
+        name: "Hiter",
+        age: 28
+    },
+    {
+        id: 2,
+        name: "Noorjahan",
+        age: 39
+    },
+    {
+        id: 3,
+        name: "Toby",
+        age: 6
+    }
+]
+
+const UsersType = new GraphQLObjectType({
+    name: 'Users',
+    description: 'List of users',
+    fields: () => ({
+        id: {type: new GraphQLNonNull(GraphQLInt)},
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLInt)}
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        users: {
+            type: new GraphQLList(UsersType),
+            description: 'List of users',
+            resolve: () => users
+        }
+    })
+});
+
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addUser: {
+            type: UsersType,
+            description: 'Add user',
+            args: {
+                name: {type: GraphQLNonNull(GraphQLString)},
+                age: {type: GraphQLNonNull(GraphQLInt)},
+            },
+            resolve: (parent, args) => {
+                const newUser = {id: users.length + 1, name: args.name, age: args.age}
+                users.push(newUser)
+                return newUser
+            }
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType,
+    mutation: RootMutationType
+});
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
